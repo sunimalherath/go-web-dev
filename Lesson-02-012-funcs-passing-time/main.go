379436e47d70a2c2a60d9ca4e706b4c8c72919d9
sunimalherath <sunimaldv@gmail.com>
2021-06-04 23:42:39 +1000

    passing func to format time in the view

---
+++ Lesson-02-012-funcs-passing-time/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+
+var fm = template.FuncMap{
+	"fdateDMY": dayMonthYear,
+}
+
+func dayMonthYear(t time.Time) string {
+	return t.Format("01-02-2006")
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
